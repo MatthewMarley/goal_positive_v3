1eec970057146b3f60e22283d2fb95a893bdaa3b
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-05-15 20:47:02 +0000

    Fixed goals controller, and made navigation available

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -9,22 +9,56 @@ class GoalsController < ApplicationController
     end
     
     def create
-        
+        @goal = Goal.new(goal_params)
+        @goal.user_id = current_user.id
+        if @goal.save
+            flash[:success] = "Your goal was successfully created!"
+            redirect_to goals_path
+        else
+            flash.now[:danger] = "Your goal was unable to save"
+            render 'new'
+        end
     end
     
     def show
-        
+        @goal = Goal.find(params[:id])
     end
     
     def edit
-        
+        @goal = Goal.find(params[:id])
     end
     
     def update
-        
+        @goal = Goal.find(params[:id])
+        if @goal.update(goal_params)
+            flash[:success] = "Goal successfully updated"
+            redirect_to goal_path(@goal)
+        else
+            flash[:danger] = "Goal unable to be updated"
+            redirect_to goal_path(@goal)
+        end
     end
     
     def destroy
-        
+        @goal = Goal.find(params[:id])
+        if @goal.destroy
+            flash[:success] = "Goal successfully destroyed"
+            redirect_to goals_path
+        else
+            flash[:danger] = "Goal unable to be deleted"
+            redirect_to goal_path(@goal)
+        end
     end
+    
+    
+    
+    private
+    def goal_params
+        params.require(:goal).permit(:name, :description) 
+    end
+    
+    def set_user
+        @user = current_user 
+    end
+    
 end
