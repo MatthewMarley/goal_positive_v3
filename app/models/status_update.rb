27cc9a91f3b7f9922a405415ea65d3ad6b7dc588
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-05-16 09:02:12 +0000

    Added model validations for status update and commentable

--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -1,6 +1,7 @@
-class Goal < ActiveRecord::Base
-    
+class StatusUpdate < ActiveRecord::Base
+
     belongs_to :user
+    belongs_to :goal
     has_many :comments, :as => :commentable
-     
+    
 end
