0b151c03a1d3ede8e7bf295c5709b073c1f2cd56
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-07-25 17:33:58 +0000

    Put Javascript in the Users Show page

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,9 +2,11 @@
 $(document).on('turbolinks:load', function() {
     $('.navbar-brand').fadeTo(2000,1);
     $('.alert').delay(4000).fadeOut(2000);
-    $("div[class^='user-goals-hover']").mouseenter(function() {
-        $("h2[class^='color-light-blue']").animate({fontSize: "32px"});
-    });
+    //$("div[class^='user-goals-hover']").mouseenter(function() {
+        //$("h2[class^='color-light-blue']").animate({fontSize: "32px"});
+});
+    
+    
     //$("div[class^='user-goals-hover']").mouseleave(function() {
     //    $("h2[class^='color-light-blue']").animate({fontSize: "30px"});
     //});
@@ -15,7 +17,7 @@ $(document).on('turbolinks:load', function() {
     //$("input[name^='user-goals-text-elements']").mouseenter(function() {
     //    $('.hide-username-1').fadeTo(2000, 1);
     //});
-});
+
 
 
 
