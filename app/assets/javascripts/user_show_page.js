b74192974a160b3bfef6bf2d307487da462ddbf6
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-07-29 18:09:20 +0000

    Further amended the about page, see 'user_show_page.js' for looping code for unhiding elements on scroll

--- app/assets/javascripts/user_show_page.js
+++ app/assets/javascripts/user_show_page.js
@@ -2,11 +2,22 @@
 $(document).on('turbolinks:load', function() {
     $('.user-show-titles').fadeTo(2000, 1);
     $('.user-goals-text-elements').mouseenter(function() {
-       $(this).find('.user-goals-description').fadeTo(1000,1); 
+       $(this).find('.user-goals-description').fadeTo(500,1); 
+    });
+    $(function() {
+       $(window).scroll(function() {
+           var nodelist = document.getElementsByTagName("p").length;
+           var paraClassName = [];
+           for (var i = 0; i < nodelist; i++) {
+               paraClassName[i] = '.about-para-' + [i+1];
+               if($(this).scrollTop() + 1000 >= $(paraClassName[i]).offset().top) {
+                   $(paraClassName[i]).fadeTo(1000, 1);
+               }
+           }
+       }); 
     });
 });
 
 
-//$("#tour").on('click', 'button', function() { 
     
     
