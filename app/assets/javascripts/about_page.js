c52fcf4ad6c4737fabd4aef59829d88bfd498040
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-07-30 21:39:06 +0000

    Changes to footer, about page jquery and requirements page

--- app/assets/javascripts/about_page.js
+++ app/assets/javascripts/about_page.js
@@ -6,10 +6,35 @@ $(document).on('turbolinks:load', function() {
            if($(this).scrollTop() + 1000 >= $('.about-overview').offset().top) {
                $('.about-overview').fadeTo(1000, 1);
            }
-           $('.about-create').mouseenter(function() {
-               $('.slider').animate({
+           // Add border and underline on hover. Use stop() to stop animations building up
+           $('.about-create').hover(function() {
+               $('.slider-create').stop().animate({
                    width: $('.h3-about-create').width()
-               }, 1000);
+               }, 500);
+           }, function() {
+               $('.slider-create').stop().animate({
+                   width: 0
+               }, 500);
+           });
+           
+           $('.about-share').hover(function() {
+               $('.slider-share').stop().animate({
+                   width: $('.h3-about-share').width()
+               }, 500);
+           }, function() {
+               $('.slider-share').stop().animate({
+                   width: 0
+               }, 500);
+           });
+           
+           $('.about-help').hover(function() {
+               $('.slider-help').stop().animate({
+                   width: $('.h3-about-help').width()
+               }, 500);
+           }, function() {
+               $('.slider-help').stop().animate({
+                   width: 0
+               }, 500);
            });
            
            
