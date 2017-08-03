20036e81a5ce8060d07621d7e242d2ffc1a38485
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-08-03 22:05:39 +0000

    Beginner to implement a search bar for users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   
   def home
-    @goals = Goal.where(status: 'Public').all.order("created_at DESC")
-    @all_goals = Goal.all
+    # @goals = Goal.where(status: 'Public').all.order("created_at DESC")
+    @all_goals = Goal.all.order("created_at DESC")
   end
   
   def about
