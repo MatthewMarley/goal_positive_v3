f54c8ae62445ababf72325240a5fe49cddb56b19
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-12-18 19:16:03 +0000

    Added Favicon

--- app/assets/javascripts/about_page.js
+++ app/assets/javascripts/about_page.js
@@ -1,6 +1,6 @@
 /* global $ */
 $(document).on('turbolinks:load', function() {
-    $('.about-para-5').fadeTo(1000, 1);
+    //$('.about-para-5').fadeTo(8000, 1);
     $(function() {
        $(window).scroll(function() {
            if($(this).scrollTop() + 1000 >= $('.about-overview').offset().top) {
@@ -56,11 +56,6 @@ $(document).on('turbolinks:load', function() {
                    width: 0
                }, 500);
            });
-           
-           
-           
-           
-          
        }); 
     });
 });    
--- app/assets/javascripts/user_show_page.js
+++ app/assets/javascripts/user_show_page.js
@@ -1,10 +1,8 @@
 /* global $ */
 $(document).on('turbolinks:load', function() {
     $('.user-show-titles').fadeTo(2000, 1);
-    $('.user-goals-hover').mouseenter(function() {
-       $(this).find('.user-goals-description').fadeTo(500,1);
-       $(this).find('.user-goals-username').fadeTo(500, 1);
-    });
+   $(this).find('.user-goals-description').fadeTo(2000,1);
+   $(this).find('.user-goals-username').fadeTo(2000, 1);
 });
 
 
