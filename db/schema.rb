22936af93db1579aa268695a0d10ad305137fe23
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-12-18 19:22:21 +0000

    Added status to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731153317) do
+ActiveRecord::Schema.define(version: 20171218192030) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170731153317) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.boolean  "admin",           default: false
+    t.string   "status"
   end
 
   create_table "votes", force: :cascade do |t|
